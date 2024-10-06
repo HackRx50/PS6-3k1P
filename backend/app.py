06c0080be5fd33e2527c399571ac6477be46d5e3
Pratyush <connect2pratyush@gmail.com>
2024-10-06 21:40:43 +0530

    order

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,13 @@
 import asyncio
+import http.client as httplib
 import json
 import os
-import uuid
 import random
 import time
-import httplib2
-import http.client as httplib
+import uuid
 
 import boto3
+import httplib2
 from botocore.exceptions import NoCredentialsError
 from database import *
 from dotenv import load_dotenv
@@ -15,15 +15,10 @@ from fastapi import (BackgroundTasks, Depends, FastAPI, File, HTTPException,
                      UploadFile)
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
-from functions import *
 from sqlalchemy.orm import Session
 
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from googleapiclient.http import MediaFileUpload
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.file import Storage
-from oauth2client.tools import run_flow
+from utils.functions import *
+from utils.youtube import *
 
 load_dotenv()
 prepare_folders()
@@ -44,20 +39,6 @@ s3_client = boto3.client('s3')
 # task status memory list because mutable, and can be passed as reference
 task_status_memory = [{}]
 
-# YouTube API constants
-CLIENT_SECRETS_FILE = "client_secrets.json"
-YOUTUBE_UPLOAD_SCOPE = "https://www.googleapis.com/auth/youtube.upload"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
-VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
-
-# YouTube upload retry constants
-MAX_RETRIES = 10
-RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, httplib.NotConnected,
-  httplib.IncompleteRead, httplib.ImproperConnectionState,
-  httplib.CannotSendRequest, httplib.CannotSendHeader,
-  httplib.ResponseNotReady, httplib.BadStatusLine)
-RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
 
 @app.get('/')
 async def hello():
@@ -151,75 +132,6 @@ async def check_task_status(task_id: str):
     if status is None:
         raise HTTPException(status_code=404, detail="Task not found")
     return {"status": status}
-
-def get_authenticated_service():
-    flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE, scope=YOUTUBE_UPLOAD_SCOPE)
-    storage = Storage("youtube-oauth2.json")
-    credentials = storage.get()
-
-    if credentials is None or credentials.invalid:
-        credentials = run_flow(flow, storage)
-
-    return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, http=credentials.authorize(httplib2.Http()))
-
-def initialize_upload(youtube, options):
-    body = dict(
-        snippet=dict(
-            title=options['title'],
-            description=options['description'],
-            tags=options['keywords'].split(",") if options['keywords'] else None,
-            categoryId=options['category']
-        ),
-        status=dict(
-            privacyStatus=options['privacyStatus']
-        )
-    )
-
-    # Download the file from S3 if it doesn't exist locally
-    local_file_path = f"vids/{options['file']}"
-    if not os.path.exists(local_file_path):
-        s3_client.download_file(s3_bucket, options['file'], local_file_path)
-
-    insert_request = youtube.videos().insert(
-        part=",".join(body.keys()),
-        body=body,
-        media_body=MediaFileUpload(local_file_path, chunksize=-1, resumable=True)
-    )
-
-    return resumable_upload(insert_request)
-
-def resumable_upload(insert_request):
-    response = None
-    error = None
-    retry = 0
-    while response is None:
-        try:
-            print("Uploading file...")
-            status, response = insert_request.next_chunk()
-            if response is not None:
-                if 'id' in response:
-                    print(f"Video id '{response['id']}' was successfully uploaded.")
-                    return response['id']
-                else:
-                    raise Exception("The upload failed with an unexpected response: %s" % response)
-        except HttpError as e:
-            if e.resp.status in RETRIABLE_STATUS_CODES:
-                error = f"A retriable HTTP error {e.resp.status} occurred:\n{e.content}"
-            else:
-                raise
-        except RETRIABLE_EXCEPTIONS as e:
-            error = f"A retriable error occurred: {e}"
-
-        if error is not None:
-            print(error)
-            retry += 1
-            if retry > MAX_RETRIES:
-                raise Exception("No longer attempting to retry.")
-
-            max_sleep = 2 ** retry
-            sleep_seconds = random.random() * max_sleep
-            print(f"Sleeping {sleep_seconds} seconds and then retrying...")
-            time.sleep(sleep_seconds)
 
 @app.post("/publish_to_youtube")
 async def publish_to_youtube(request: dict):
